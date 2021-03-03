13ec8749b054fe7ba9af43385e89a02bb62f0a41
Jake Mander <Jake@chilli-is.co.uk>
2021-03-03 15:27:11 +0000

    Completed the "Creating the Vue App Secrion"

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const product = 'Socks'
+const app = Vue.createApp(
+    {
+        //  ES6 Shorthand - Use product : function() for ES5 Lower Compatibility. 
+        data() {
+            return {
+                product: 'Socks',
+                description: 'A lovely pair of socks.'
+            }
+        }
+    }
+)
