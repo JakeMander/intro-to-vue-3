b51841c207cec2baa10d84cb5a8681af589a99ed
Jake Mander <Jake@chilli-is.co.uk>
2021-03-04 15:28:16 +0000

    Completed Event Handling

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const app = Vue.createApp(
-    {
+const app = Vue.createApp({
         //  ES6 Shorthand - Use product : function() for ES5 Lower Compatibility. 
         data() {
             return {
+                cartItems: 0,
                 description: 'A lovely pair of socks.',
                 details: ['50% Cotton', '30% Wool', '20% Polyester'],
                 imageUrl: "./assets/images/socks_green.jpg",
@@ -12,9 +12,23 @@ const app = Vue.createApp(
                 shopLinkUrl: "https://www.sockshop.co.uk/",
                 sizes: ['Small', 'Medium', 'Large'],
                 variants: [
-                    {id: 2234, colour: 'Green'},
-                    {id: 2235, colour: 'Blue'}
+                    {id: 2234, colour: 'Green', imageUrl: ".\\assets\\images\\socks_green.jpg"},
+                    {id: 2235, colour: 'Blue', imageUrl: ".\\assets\\images\\socks_blue.jpg"}
                 ]
+            }
+        },
+        methods: {
+            addToCart() {
+                this.cartItems += 1;
+            },
+            removeFromCart() {
+                cartItems = this.cartItems;
+                
+                if (cartItems > 0)
+                    this.cartItems -= 1;
+            },
+            updateImage(newUrl) {
+                this.imageUrl = newUrl;
             }
         }
     }
