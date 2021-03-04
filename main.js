dd6383048a72613870feb36195aa77569511b26c
Jake Mander <Jake@chilli-is.co.uk>
2021-03-04 12:36:49 +0000

    Completed List Rendering

--- main.js
+++ main.js
@@ -3,12 +3,18 @@ const app = Vue.createApp(
         //  ES6 Shorthand - Use product : function() for ES5 Lower Compatibility. 
         data() {
             return {
-                product: 'Socks',
                 description: 'A lovely pair of socks.',
+                details: ['50% Cotton', '30% Wool', '20% Polyester'],
                 imageUrl: "./assets/images/socks_green.jpg",
-                shopLinkUrl: "https://www.sockshop.co.uk/",
+                inventory: 19,
                 onSale: true,
-                inventory: 0
+                product: 'Socks',
+                shopLinkUrl: "https://www.sockshop.co.uk/",
+                sizes: ['Small', 'Medium', 'Large'],
+                variants: [
+                    {id: 2234, colour: 'Green'},
+                    {id: 2235, colour: 'Blue'}
+                ]
             }
         }
     }
